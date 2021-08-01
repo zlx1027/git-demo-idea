3b70afe1b7ffb170a7ec0209e9a0c63947fd3421
zhanglixia <2873651925@qq.com>
2021-08-01 23:30:43 +0800

    第四次提交

--- src/main/java/com/antiguigu/gitdemo/entity/User.java
+++ src/main/java/com/antiguigu/gitdemo/entity/User.java
@@ -4,4 +4,10 @@ public class User {
     private Integer id;
     private String username;
     private String password;
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
 }
